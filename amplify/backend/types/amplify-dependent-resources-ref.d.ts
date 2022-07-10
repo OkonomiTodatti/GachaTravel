4427ab5bda97e660c7efc0fa399ab815a69446cb
toasahi <asa1313to@gmail.com>
2022-07-10 14:38:20 +0900

    amplify api追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    gachatravel6d2dc26b: {
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      UserPoolId: 'string';
+      UserPoolArn: 'string';
+      UserPoolName: 'string';
+      AppClientIDWeb: 'string';
+      AppClientID: 'string';
+    };
+  };
+  api: {
+    gachatravel: {
+      GraphQLAPIKeyOutput: 'string';
+      GraphQLAPIIdOutput: 'string';
+      GraphQLAPIEndpointOutput: 'string';
+    };
+  };
+};
