64fb24676f138804ec6a5420756b1f2a0003bfa3
toasahi <asa1313to@gmail.com>
2022-07-12 16:23:19 +0900

    Animationページを追加

--- src/components/pages/GachaAnimationPage.js
+++ src/components/pages/GachaAnimationPage.js
@@ -1,9 +1,15 @@
 import React, { memo, useEffect, useRef, useState } from 'react';
 import LottieView from 'lottie-react-native';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listStocks } from '../../graphql/queries';
+import { createStock } from '../../graphql/mutations';
+import { useLoginUser } from '../../provider/LoginUserProvider';
 
-export const GachaAnimationPage = memo(() => {
+export const GachaAnimationPage = memo((props) => {
+  const { navigation } = props;
   const lottieRef = useRef();
   const [gachaState, setGachaState] = useState(true);
+  const { loginUser } = useLoginUser();
   const resetAnimation = () => {
     if (lottieRef.current) {
       lottieRef.current.reset();
@@ -19,6 +25,27 @@ export const GachaAnimationPage = memo(() => {
       }
     }
   }, [lottieRef, gachaState]);
+
+  setTimeout(() => {
+    // setGachaState(false);
+    fetchCreateStock({
+      user_id: loginUser,
+      ticket_id: Math.round(Math.random() * 3 + 1),
+      plan_id: '1',
+      status: 'Before',
+      people: '1',
+    }).then(() => navigation.navigate('GachaPage'));
+    // navigation.navigate('GachaPage');
+  }, 5.05 * 1000);
+
+  async function fetchCreateStock(stockDatas) {
+    try {
+      await API.graphql(graphqlOperation(createStock, { input: stockDatas }));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <LottieView
       ref={lottieRef}
