8d093a65bb39a1f767782f8389849fc3eeadea81
toasahi <asa1313to@gmail.com>
2022-07-21 14:44:01 +0900

    旅行先モーダルを変更

--- src/components/Modal/TravelModal.js
+++ src/components/Modal/TravelModal.js
@@ -1,26 +1,15 @@
-import React, { memo } from 'react';
-import { Text, View } from 'react-native';
+import React, { memo, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Light from '../../assets/light.svg';
 import Japan from '../../assets/japan.svg';
 import BGrid from '../../assets/Bgrid.svg';
-import { ResultTag } from '../Gacha/ResultTag';
+import PageButton from '../../assets/travel_arrow.svg';
 
 export const TravelModal = memo((props) => {
-  const { nextButton } = props;
+  const { nextButton = false, onPressUp, onPressDown, type = 'primary' } = props;
   return (
-    <View>
-      <View
-        style={{
-          marginTop: 5,
-          backgroundColor: '#20bdbd',
-          borderColor: '#137878',
-          borderWidth: 3,
-          paddingHorizontal: 3,
-          paddingBottom: 3,
-          borderRadius: 40,
-          position: 'relative',
-        }}
-      >
+    <View style={[styles[`container_${type}`]]}>
+      <View style={styles.card}>
         <Light style={{ top: 10, left: 20, position: 'absolute' }} />
         <Text
           style={{
@@ -72,10 +61,73 @@ export const TravelModal = memo((props) => {
             <View style={{ marginTop: 40, marginLeft: 'auto', marginRight: 'auto' }}>
               <Japan />
             </View>
-            {nextButton ? <View style={{ height: 200 }}></View> : <View></View>}
+            {nextButton ? (
+              <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 16 }}>
+                <Pressable onPress={onPressDown}>
+                  <PageButton />
+                </Pressable>
+                <View
+                  style={{
+                    backgroundColor: '#20bdbd',
+                    borderWidth: 1,
+                    borderColor: '#137878',
+                    borderRadius: 5,
+                    marginTop: 6,
+                    height: 30,
+                    shadowColor: '#137878',
+                    shadowOffset: {
+                      width: 0,
+                      height: 2,
+                    },
+                    shadowOpacity: 1,
+                    shadowRadius: 0,
+                    elevation: 1,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      color: '#fff',
+                      paddingHorizontal: 14,
+                      lineHeight: 26,
+                      fontWeight: 'bold',
+                      letterSpacing: 3,
+                      fontFamily: 'Noto Sans',
+                    }}
+                  >
+                    1/6
+                  </Text>
+                </View>
+                <Pressable onPress={onPressUp}>
+                  <PageButton style={{ transform: [{ rotate: '60deg' }] }} />
+                </Pressable>
+              </View>
+            ) : (
+              <View></View>
+            )}
           </View>
         </View>
       </View>
     </View>
   );
+});
+
+const styles = StyleSheet.create({
+  container_primary: {
+    marginTop: 0,
+  },
+
+  container_detail: {
+    marginTop: 10,
+  },
+
+  card: {
+    backgroundColor: '#20bdbd',
+    borderColor: '#137878',
+    borderWidth: 3,
+    paddingHorizontal: 3,
+    paddingBottom: 3,
+    borderRadius: 40,
+    position: 'relative',
+  },
 });
