35decd46bd9b92d5aee7947e0f9a529a7fb32933
toasahi <asa1313to@gmail.com>
2023-02-05 15:28:30 +0900

    constantディレクトリに移動

---
+++ src/constants/data.js
@@ -0,0 +1,22 @@
+export const peopleData = ['1人', '2人', '3人', '4人'];
+export const stayDaysData = ['1日', '2日', '3日', '4日'];
+export const travelPlanData = ['記念館巡り', '夏旅行', '冬旅行', '秋旅行', 'ラーメン旅行'];
+export const airPlaneClassData = ['エコノミークラス', 'ビジネスクラス', 'ファーストクラス'];
+export const prefectureData = {
+  1: {
+    prefecture: '北海道',
+    kana: 'HOKAIDO',
+  },
+  2: {
+    prefecture: '東京都',
+    kana: 'TOKYO',
+  },
+  3: {
+    prefecture: '大阪府',
+    kana: 'OSAKA',
+  },
+  4: {
+    prefecture: '長崎県',
+    kana: 'NAGASAKI',
+  },
+};
